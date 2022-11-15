8ccaea23afdc2af9ba3915fdf744ea6a8d97a832
nkorojoseph <nkorojoseph@yahoo.com>
2022-11-15 02:32:27 +0100

    little changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import './styles/main.css'
 import  {TipForm}  from './Components/TipForm';
 
-
-
-
 function App() {
   return (
     <div className="App">
       <section className="header">
         <div className="w3-container">
-            <h2>SPLI<span>TTER</span></h2>
+            <h2>SPLITTER</h2>
         </div>
       </section>
       {<TipForm />}
